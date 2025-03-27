90c6dce171535f799a1ba8d70327b45c4d984dd0
Matp1 <matheus.paivaads@gmail.com>
2025-03-27 15:35:26 -0300

    corrige caseSensitive

--- trevi/src/pages/slideShow/slideShow.jsx
+++ trevi/src/pages/slideShow/slideShow.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Slideshow.css";
+import "./slideshow.css";
 
 const Slideshow = ({ slides }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
